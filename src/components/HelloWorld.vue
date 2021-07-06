4cd6bd77a70082b927bcf083b98204ef6096faea
La-wii <sentilavale@gmail.com>
2021-07-06 16:51:01 +0200

    header

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,25 @@
 <template>
-  <header>
+  <header class="clearfix">
 
     <div class="logo">
-
+      <img src="../img/dc-logo.png" alt="">
     </div>
 
     <nav>
-
+      <ul>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Comics</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">Tv</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Collectibles</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">Fans</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Shop</a></li>
+      </ul>
     </nav>
-    
+
   </header>
 
 </template>
@@ -24,18 +35,39 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+header{
+    .clearfix::after{
+      display: table;
+      content: '';
+      clear: both;
+    }
+    .logo{
+      float: left;
+      margin-left: 100px;
+    }
+    nav{
+      float: right;
+      ul {
+      justify-content: flex-end;
+      list-style-type: none;
+      padding: 0;
+      margin-right: 100px;
+        li {
+        display: inline-block;
+        margin: 0 10px;
+          &:hover{
+            padding-bottom: 30px;
+            border-bottom: 5px solid blue;
+          }
+          a {
+          color: black;
+          text-decoration: none;
+          }
+        }
+      }
+    }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+
+
 </style>
